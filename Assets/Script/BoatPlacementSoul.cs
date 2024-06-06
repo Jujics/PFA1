b32adfd3a4a344f69124fc33755908055f2f7daf
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-06-06 16:13:37 +0200

    Its somewhat working

--- Assets/Script/BoatPlacementSoul.cs
+++ Assets/Script/BoatPlacementSoul.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BoatPlacementSoul : MonoBehaviour, IDropHandler
 {
     public GameObject[] SlotPos;
+    public GameObject ParentOf;
+    private DragAndDrop IdTransfer;
+    private bool FoundPlace = false;
+    private IsPosOpen IsOp;
+
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Droped");
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
-                GetComponent<RectTransform>().anchoredPosition;
+            IdTransfer = eventData.pointerDrag.GetComponent<DragAndDrop>();
+            int i = 0;
+            while (FoundPlace == false || i == SlotPos.Length)
+            {
+
+                IsOp = SlotPos[i].GetComponent<IsPosOpen>();
+                if (IsOp.isEmpty == false)
+                {
+                    eventData.pointerDrag.transform.parent = this.gameObject.transform;
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
+                        SlotPos[i].GetComponent<RectTransform>().anchoredPosition;
+                    IdTransfer.CurrentBoatPlacementId = IsOp.id;
+                    FoundPlace = true;
+                    IsOp.isEmpty = true;
+                }
+                else
+                {
+                    i += 1;
+                }
+            }
+
             eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            FoundPlace = false;
         }
     }
 }
--- Assets/Script/DragAndDrop.cs
+++ Assets/Script/DragAndDrop.cs
@@ -1,13 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,IEndDragHandler, IDragHandler
 {
+   public int CurrentBoatPlacementId;
+   public GameObject Boat;
+   public GameObject MainCan;
+   public IsPosOpen SetBackToFalse;
    private RectTransform rectTransform;
    private CanvasGroup canvasGroup;
+
+   private void Start()
+   {
+      CurrentBoatPlacementId = 18;
+   }
+
    private void Awake()
    {
       rectTransform = GetComponent<RectTransform>();
@@ -17,6 +28,9 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
    public void OnPointerDown(PointerEventData eventData)
    {
       Debug.Log("Onpointerdown");
+      GameObject ZonePl = Boat.transform.GetChild(CurrentBoatPlacementId).gameObject;
+      SetBackToFalse = ZonePl.GetComponent<IsPosOpen>();
+      SetBackToFalse.isEmpty = false;
    }
 
    public void OnBeginDrag(PointerEventData eventData)
