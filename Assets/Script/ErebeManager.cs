08c1b40f2897d2684345e3d1c138d629148b7d5e
SWDestiny <162432591+SWDestiny@users.noreply.github.com>
2024-06-11 14:29:18 +0200

    ErebeEssaie

--- Assets/Script/ErebeManager.cs
+++ Assets/Script/ErebeManager.cs
@@ -1,8 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ErebeManager : MonoBehaviour
+using UnityEngine.EventSystems;
+public class ErebeManager : MonoBehaviour, IDropHandler
 {
+    private Queue<GameObject> itemQueue = new Queue<GameObject>();
+    public RectTransform[] slots; 
     
+    public RectTransform door; 
+    public float doorCooldown = 2f;
+
+    private bool doorOpen = true;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Dropped");
+
+        if (eventData.pointerDrag != null)
+        {
+            GameObject droppedItem = eventData.pointerDrag;
+            if (itemQueue.Count < slots.Length)
+            {
+                itemQueue.Enqueue(droppedItem);
+                UpdateQueuePositions();
+                droppedItem.GetComponent<CanvasGroup>().blocksRaycasts = false;
+            }
+            else
+            {
+                Debug.Log("Queue is full");
+            }
+        }
+    }
+
+    private void UpdateQueuePositions()
+    {
+        int index = 0;
+        foreach (var item in itemQueue)
+        {
+            item.transform.SetParent(slots[index]);
+            item.GetComponent<RectTransform>().anchoredPosition = slots[index].anchoredPosition;
+            index++;
+        }
+
+        if (doorOpen && itemQueue.Count > 0)
+        {
+            //StartCoroutine(ProcessNextItem());
+        }
+    }
+
+    /*private IEnumerator ProcessNextItem()
+    {
+        doorOpen = false;
+        GameObject currentItem = itemQueue.Dequeue();
+        currentItem.GetComponent<RectTransform>().anchoredPosition = door.anchoredPosition;
+        currentItem.SetActive(false);
+        Debug.Log("Door is closed for " + doorCooldown + " seconds.");
+        yield return new WaitForSeconds(doorCooldown);
+        Debug.Log("Door is open.");
+
+        doorOpen = true;
+        
+        UpdateQueuePositions();
+        
+    }*/
 }
