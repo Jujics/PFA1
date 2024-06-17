c66b057bbb2c9c46075520ea351e1cede6d7f46d
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-06-17 14:54:31 +0200

    push things

---
+++ Assets/Script/EnemiesDataHold.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyScoreData", menuName = "EnnemyScoreData")]
+public class EnemiesDataHold : ScriptableObject
+{
+    public float[] EnemyOne;
+    public float[] EnemyTwo;
+    public float[] EnemyThree;
+    public float[] EnemyFour;
+    public float[] EnemyFive;
+    public float[] EnemySix;
+}
